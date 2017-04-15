38ffbe9b4e4d30c32a3649142f54c2f0fd5b225d
Tomoya Koyanagi <tomk79@gmail.com>
2017-04-16 00:07:17 +0900

    xlsx読み込みを仮実装

--- php/parser/xlsx.php
+++ php/parser/xlsx.php
@@ -36,12 +36,81 @@ class parser_xlsx{
 			$objPHPExcel->setActiveSheetIndex($sheetIndex);
 			$objSheet = $objPHPExcel->getActiveSheet();
 
-			$table_name = $objSheet->getCell('B1')->getCalculatedValue();
-			@$definition->tables->{$table_name} = json_decode('{}');
-			@$definition->tables->{$table_name}->sheet_label = $objSheet->getTitle();
+			$sheetData = $this->parse_sheet( $objSheet );
+			@$definition->tables->{$sheetData->table_name} = $sheetData;
+
 		}
 
 		return $definition;
+	}
+
+
+	/**
+	 * シートを解析する
+	 * @param  object $objSheet PHPExcel Sheet Instance
+	 * @return object           Parsed sheet data
+	 */
+	private function parse_sheet( $objSheet ){
+		$parsed = json_decode('{}');
+
+		$table_name = $objSheet->getCell('B1')->getCalculatedValue();
+
+		@$parsed->sheet_label = $objSheet->getTitle();
+		@$parsed->table_name = $table_name;
+		@$parsed->table_definition = json_decode('{}');
+
+		// --------------------
+		// 定義列を読み取り
+		$col_define = array();
+		$col = 'A';
+		$skip_count = 0;
+		while(1){
+			$def_key = $objSheet->getCell($col.'3')->getCalculatedValue();
+			if(!strlen($def_key)){
+				$skip_count ++;
+				$col ++;
+				if( $skip_count > 20){
+					break;
+				}
+				continue;
+			}
+			$skip_count = 0;
+
+			$col_define[$def_key] = array(
+				'key'=>trim($def_key),
+				'col'=>$col,
+				// 'name'=>$def_name,
+			);
+
+			if( @strlen($mergeInfo[$col]) ){
+				$mergeStartCol = $mergeInfo[$col];
+				while( strcmp($mergeStartCol, $col) ){
+					$col ++;
+				}
+			}else{
+				$col ++;
+			}
+		}
+
+		// var_dump($col_define);
+
+		// --------------------
+		// テーブル定義を読み取り
+		$row_number = 4;
+		while( 1 ){
+			$col = json_decode('{}');
+			foreach($col_define as $col_def_row){
+				$col->{$col_def_row['key']} = $objSheet->getCell($col_def_row['col'].$row_number)->getCalculatedValue();
+			}
+			if(!@strlen($col->cell_name)){
+				break;
+			}
+			@$parsed->table_definition->{$col->cell_name} = $col;
+			$row_number ++;
+			continue;
+		}
+
+		return $parsed;
 	}
 
 }
