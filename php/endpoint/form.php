80b136c84b8d95e0bf637d9cc2c13083d45f66ac
Tomoya Koyanagi <tomk79@gmail.com>
2017-05-01 19:15:08 +0900

    Form Endpoint: テーブル中の行の一覧画面を追加

--- php/endpoint/form.php
+++ php/endpoint/form.php
@@ -86,6 +86,8 @@ class endpoint_form{
 			$this->table_definition = $this->exdb->get_table_definition($this->options['table']);
 		}
 
+
+		// Twig テンプレートエンジンを準備
 		$loader = new \Twig_Loader_Filesystem(__DIR__.'/../templates/');
 		$this->twig = new \Twig_Environment($loader, array(
 			// 'cache' => $this->exdb->conf()->path_cache_dir.'/twig_cache/',
@@ -121,6 +123,14 @@ class endpoint_form{
 		}elseif( $this->options['method'] == 'GET' ){
 			// --------------------------------------
 			// 画面表示系リクエスト
+
+			if( !strlen($this->options['id']) ){
+				// ID無指定の場合、一覧情報を返す
+				echo $this->page_list($this->options['table']);
+				return null;
+			}else{
+				// ID指定がある場合、詳細情報1件を返す
+			}
 
 		}
 
@@ -158,7 +168,48 @@ class endpoint_form{
 		// var_dump($table_list);
 		$rtn = $this->wrap_theme($rtn);
 		return $rtn;
-	}
+	} // page_table_list()
+
+
+	/**
+	 * 行の一覧画面を描画
+	 * @return String HTML Source Code
+	 */
+	private function page_list( $table_name ){
+		// var_dump($table_name);
+		$list = $this->exdb->select($table_name, array(), $this->query_options);
+		$count = $this->exdb->count($table_name, array());
+
+		// var_dump($this->table_definition->system_columns);
+		if( count($this->table_definition->system_columns->password) ){
+			foreach( $list as $key=>$val ){
+				foreach($this->table_definition->system_columns->password as $column_name){
+					unset($list[$key][$column_name]);
+				}
+			}
+		}
+		$original_list = $list;
+		$list = array();
+		foreach( $original_list as $row ){
+			$tmp_row = array();
+			$tmp_row['label'] = $row[$this->table_definition->system_columns->id->column_name];
+			$tmp_row['href'] = $_SERVER['SCRIPT_NAME'].'/'.$table_name.'/'.$row['user_id'].'/';
+			$tmp_row['val'] = $row;
+			array_push($list, $tmp_row);
+		}
+
+		$rtn = $this->twig->render(
+			'form_list.html',
+			array(
+				'count'=>$count,
+				'list'=>$list,
+			)
+		);
+		// var_dump($table_list);
+		$rtn = $this->wrap_theme($rtn);
+		return $rtn;
+	} // page_list()
+
 
 	/**
 	 * HTMLのテーマでラップする
